6521645364a1d75d96641e425f420cd0904fc665
jcryptodev <jdl.lourens@gmail.com>
2022-08-10 17:53:33 +0200

    created left side

---
+++ src/components/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import RecipeList from './RecipeList';
+import '../css/app.css';
+import { v4 as uuidv4 } from 'uuid';
+
+export const RecipeContext = React.createContext();
+
+function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete,
+  };
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{ id: uuidv4(), name: 'Name', amount: '1 tbs' }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((receipe) => receipe.id !== id));
+  }
+
+  return (
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList
+        recipes={recipes}
+        handleRecipeAdd={handleRecipeAdd}
+        handleRecipeDelete={handleRecipeDelete}
+      />
+    </RecipeContext.Provider>
+  );
+}
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions:
+      '1. Put salt on chicken\n2. Put chicken in oven\n3. eat the chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Lbs',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Paprika on Pork\n2. Put Pork in oven\n3. eat the pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds',
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs',
+      },
+    ],
+  },
+];
+
+export default App;
